5c46cad698e4c76f01dde117ff101dab6c025ccd
mateusgomes01 <melo.mateus@proton.me>
2022-09-08 20:13:51 -0300

    Solve 03_reverseString/reverseString.js

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(phrase) {
+    let revertedPhrase = "";
 
+    for( let i = phrase.length - 1 ; i >= 0 ; i-- ){
+        revertedPhrase = revertedPhrase + phrase[i];
+    }
+
+    return revertedPhrase;
 };
 
 // Do not edit below this line
