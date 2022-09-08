c6a7c0ae6baaa36c577d3eeb040f16d2b4a284e2
mateusgomes01 <melo.mateus@proton.me>
2022-09-08 10:35:39 -0300

    Solve 02_repeatString/repeatString.js

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(phrase, n) {
+    let i = n;
+    let newPhrase = '';
 
+    if(n<0){
+        return 'ERROR';
+    }
+
+    while( i-->0 ){
+        newPhrase = newPhrase + phrase;
+    }
+
+    return newPhrase;
 };
 
 // Do not edit below this line
