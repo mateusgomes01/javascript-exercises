2836e57090f398133f94f26c0b94fec3ed7d9f73
mateusgomes01 <melo.mateus@proton.me>
2022-09-08 20:50:04 -0300

    Solve 04_removeFromArray/removeFromArray.js

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr) {
+    for( let j = 1 ; j < arguments.length ; j++ ){
+        for( let i = 0 ; i < arr.length ; i++ ){
+            if( arr[i] === arguments[j] ){
+                arr.splice(i, 1);
+            }
+        }
+    }
 
+    return arr;
 };
 
 // Do not edit below this line
