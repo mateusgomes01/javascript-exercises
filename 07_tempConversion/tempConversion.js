412d6ae14898ff841cf53ca022924f94e56c7ff5
mateusgomes01 <melo.mateus@proton.me>
2022-09-09 11:10:08 -0300

    Solve 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,15 @@
-const ftoc = function() {
+const round = function(value, precision) {
+  let multiplier = Math.pow(10, precision || 0);
+  return Math.round(value * multiplier) / multiplier;
+}
+
+const ftoc = function(fahrenheit) {
+  return round((fahrenheit - 32) * (5/9), 1);
 
 };
 
-const ctof = function() {
-
+const ctof = function(celsius) {
+  return round((celsius * (9/5)) + 32, 1);
 };
 
 // Do not edit below this line
