0f56c96bb5fbfeb418fa1a7c224e13f3a5ee75b2
mateusgomes01 <melo.mateus@proton.me>
2022-09-09 10:39:18 -0300

    Solve 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    let sum = 0;
 
+    if( typeof(a) !== 'number' || typeof(b) !== 'number'){ // I don't think that this is good code practice
+        return "ERROR";
+    }
+
+    if(a<0 || b<0){ // I don't think that this is good code practice
+        return "ERROR";
+    }
+    
+    let start = Math.min(a,b);
+    let end = a === start ? b : a;
+
+    for( let i = start ; i <= end ; i++ ){
+        sum = sum + i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
