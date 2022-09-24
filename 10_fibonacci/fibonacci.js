38dd7a41515c847dd8c5226f00f1e0af855c87a8
mateusgomes01 <melo.mateus@proton.me>
2022-09-24 17:12:00 -0300

    Solve 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    if(n<0){
+        return "OOPS";
+    }
 
+    let curr = 1, prev = 1;
+    for( let i = 2 ; i < n ; i++ ){
+        let temp = curr;
+        curr = curr + prev;
+        prev = temp;
+    }
+
+    return curr;
 };
 
 // Do not edit below this line
