470950dc2059f1d051f0a64eef467ffe4feae4e6
mateusgomes01 <melo.mateus@proton.me>
2022-09-24 16:38:27 -0300

    Solve 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum = sum + arr[i];
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  let prod = 1;
+  for (let i = 0; i < arr.length; i++) {
+    prod = prod * arr[i];
+  }
+  return prod;
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function (n) {
+  let i = 1;
+  let prod = 1;
+  while (i <= n) {
+    prod = prod * i;
+    i++;
+  }
+
+  return prod;
 };
 
 // Do not edit below this line
