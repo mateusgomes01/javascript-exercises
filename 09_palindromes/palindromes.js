a04f3d04afe535b8eaad34b18319d1a2a23b5ee9
mateusgomes01 <melo.mateus@proton.me>
2022-09-24 16:56:58 -0300

    Solve 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
+const palindromes = function (phrase) {
+    phrase = phrase.toLowerCase().replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+    phrase = phrase.replace(/\s/g,"");
+    for(let i = 0, j=phrase.length-1 ; i<(j/2) ; i++, j-- ){
+        if(phrase[i] !== phrase[j]){
+            return false;
+        }
+    }
 
+    return true;
 };
 
 // Do not edit below this line
